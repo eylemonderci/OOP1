b576eae36233de2cb0f8dcae60458d3b4d3e20ce
Eylem Önderci <Eylem nderci@DESKTOP-K5GO75U>
2024-02-17 16:59:17 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Channels;
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+
+            MyList<string> isimler=new MyList<string>(); // Generic yapılara bir örnek.
+            isimler.Add("Kerem");
+
+
+        }
+    }
+}
